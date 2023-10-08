a0f13f458cd963ac348a08df30139370a7d34611
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-10-08 19:01:44 +0900

    update: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        src="/sample.jpg"
+        alt=""
+        width={670}
+        height={586}
+        priority
+      />
+    </main>
+  )
+}
